5de6de5fe66ab00d8c77f5ac3d9a8d9b1a775f89
Traven Gonzales <travengonzales@gmail.com>
2023-08-14 15:16:17 -0700

    feat(file-reading): Added logic to parse through simple feedback csv

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
 const fs = require("fs");
 const csv = require("csv-parser");
 
+const headers = [
+    "feedback_id",
+    "user_id",
+    "feedback"
+];
+
+// Main Driver
 (() => {
-    console.log(process.argv);
+    const feedbackCsvFile = process.argv[2];
+
+    const feedbackStream = fs.createReadStream(feedbackCsvFile, "utf-8")
+        
+    feedbackStream.on("error", (error) => {
+        if (error.code === "ENOENT") {
+            console.log(`File ${feedbackCsvFile} does not exist, please pass valid file`);
+        } else {
+            console.log(error)
+        }
+    });
+    
+    feedbackStream
+        .pipe(csv({
+            headers: headers,
+            skipLines: 1
+        }))
+        .on("data", (chunk) => {
+            console.log(chunk);
+        });
+
+    feedbackStream
+        .on("end", () => console.log(`${feedbackCsvFile} has been processed.`));
+
+    feedbackStream
+        .on("close", () => console.log(`${feedbackCsvFile} stream has been closed.`))
 })()
