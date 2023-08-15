be61cd34680ed631fd0546c726632098d5893c2c
Traven Gonzales <travengonzales@gmail.com>
2023-08-15 14:53:29 -0700

    feat(average-sentiment): Added calculation of average sentiment calculated from JSON file

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const path = require("path");
 const JSONStream = require("JSONStream");
 const { SentimentManager } = require("node-nlp");
 const { createWorker } = require("tesseract.js");
-
+const { default: BigNumber } = require("bignumber.js");
 
 // File Utils
 /**
@@ -12,6 +12,29 @@ const { createWorker } = require("tesseract.js");
  */
 const isJson = (file) => {
     return path.extname(file).toLowerCase() === ".json";
+}
+
+// Sentiment Analysis Utils
+/**
+ * @param {BigNumber[]} sentimentScores An array of computed sentiment
+ * scores from the JSON file of feedback
+ * @returns {BigNumber} The average sentiment of the sentimentScores
+ */
+const calcAverageSentiment = (sentimentScores) => {
+    const scoresAmt = sentimentScores.length;
+
+    // Avoids divide by 0 error
+    if (scoresAmt === 0) {
+        return 0;
+    }
+
+    let averageSentiment = new BigNumber(0);
+
+    sentimentScores.forEach(score => 
+        averageSentiment = score.plus(averageSentiment)
+    );
+
+    return averageSentiment.dividedBy(scoresAmt).toFixed();
 }
 
 // Main Driver
@@ -47,23 +70,33 @@ const isJson = (file) => {
             `File ${feedbackJsonFile} does not exist, please pass a valid file`
         );
         } else {
-        console.log(error);
+            console.log(error);
         }
     });
+
+    const sentimentScores = [];
 
     const sentiment = new SentimentManager();
 
     // Assumes the main JSON will only have a singular member which will
     // be structured as an array of all feedback entries, wherein the
     // user's written feedback will be in a member called "feedback"
-    feedbackStream.pipe(JSONStream.parse("*")).on("data", (chunk) => {
-        chunk.forEach((feedbackEntry) => {
-        sentiment.process("en", feedbackEntry.feedback).then((result) =>
-            console.log({
-            ...feedbackEntry,
-            sentiment_analysis: result,
-            })
-        );
+    feedbackStream.pipe(JSONStream.parse("*"))
+        .on("data", chunk => {
+            chunk.forEach(feedbackEntry => {
+                sentiment.process("en", feedbackEntry.feedback).then(result => {
+                    sentimentScores.push(new BigNumber(result.score));
+
+                    console.log({
+                        ...feedbackEntry,
+                        sentiment_analysis: result
+                    });
+                });
+            });
         });
-    });
+
+    feedbackStream
+        .on("close", () => {
+            console.log(calcAverageSentiment(sentimentScores));
+        })
 })();
