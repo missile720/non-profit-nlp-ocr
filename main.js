0056f619a2efd854d456ddbb525c7a64e111b5e6
Traven Gonzales <travengonzales@gmail.com>
2023-08-15 16:00:39 -0700

    refactor(sentiment-utils): Refactored sentiment analysis to be in its own separated utils file

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 const JSONStream = require("JSONStream");
-const { SentimentManager } = require("node-nlp");
 const { createWorker } = require("tesseract.js");
-const { default: BigNumber } = require("bignumber.js");
+
+const SentimentStatisticTracker = require("./utils/sentimentAnalysis.js");
 
 // File Utils
 /**
@@ -12,43 +12,6 @@ const { default: BigNumber } = require("bignumber.js");
  */
 const isJson = (file) => {
     return path.extname(file).toLowerCase() === ".json";
-}
-
-// Sentiment Analysis Utils
-/**
- * @param {BigNumber[]} sentimentScores An array of computed sentiment
- * scores from the JSON file of feedback
- * @returns {BigNumber} The average sentiment of the sentimentScores
- */
-const calcAverageSentiment = (sentimentScores) => {
-    const scoresAmt = sentimentScores.length;
-
-    // Avoids divide by 0 error
-    if (scoresAmt === 0) {
-        return 0;
-    }
-
-    let averageSentiment = new BigNumber(0);
-
-    sentimentScores.forEach(score => 
-        averageSentiment = score.plus(averageSentiment)
-    );
-
-    return averageSentiment.dividedBy(scoresAmt);
-}
-
-/**
- * Returns the vote of a sentiment score, whether or not a computed
- * sentiment was neutral, positive, or negative, based on senticon scoring,
- * the lexicon scoring system used by the SentimentManager to compute
- * sentiment per message
- * @param {BigNumber} sentimentScore A sentiment score
- * @returns {string} The vote of the score based on senticon scoring
- */
-const getSentimentVote = (sentimentScore) => {
-    if (sentimentScore.isEqualTo(0)) return "neutral";
-
-    return sentimentScore.isGreaterThan(0) ? "postive" : "negative";
 }
 
 // Main Driver
@@ -88,30 +51,19 @@ const getSentimentVote = (sentimentScore) => {
         }
     });
 
-    const sentimentScores = [];
-
-    const sentiment = new SentimentManager();
+    const sentiment = new SentimentStatisticTracker();
 
     // Assumes the main JSON will only have a singular member which will
     // be structured as an array of all feedback entries, wherein the
     // user's written feedback will be in a member called "feedback"
     feedbackStream.pipe(JSONStream.parse("*"))
         .on("data", chunk => {
-            chunk.forEach(feedbackEntry => {
-                sentiment.process("en", feedbackEntry.feedback).then(result => {
-                    sentimentScores.push(new BigNumber(result.score));
-
-                    console.log({
-                        ...feedbackEntry,
-                        sentiment_analysis: result
-                    });
-                });
-            });
+            chunk.forEach(feedbackEntry => sentiment.process(feedbackEntry));
         });
 
     feedbackStream
         .on("close", () => {
-            const averageSentiment = calcAverageSentiment(sentimentScores);
-            console.log(`Average sentiment of dataset is ${getSentimentVote(averageSentiment)} with a score of ${averageSentiment.toFixed()}`);
+            const averageSentiment = sentiment.calcAverageSentiment(sentiment.sentimentScores);
+            console.log(`Average sentiment of dataset is ${sentiment.getSentimentVote(averageSentiment)} with a score of ${averageSentiment.toFixed()}`);
         })
 })();
