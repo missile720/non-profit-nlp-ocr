22dc866f14399d36f0d0e2ad3dab15783166327e
Traven Gonzales <travengonzales@gmail.com>
2023-08-30 16:00:19 -0700

    chore(organizing-methods): Reordered methods in code for SentimentStatisticTracker and made minor edits to getters

--- utils/sentimentAnalysis.js
+++ utils/sentimentAnalysis.js
@@ -15,7 +15,61 @@ class SentimentStatisticTracker {
         this.conversationScores = new Map();
         this.language = "en";
     }
+    
+    // --------MAIN METHODS--------
+    /**
+     * Processes an array of messages for a conversation and tracks 
+     * the overall sentiment from the conversation and participants in 
+     * the conversation
+     * @param {string} conversationId The id of the conversation, 
+     * specifying the participants of the conversation as well as the 
+     * identifier for the conversation in this.conversationScores
+     * @param {Object[]} messages An array of message objects for the
+     * sentimentManager and SentimentStatTracker to process. Each
+     * member consists of
+     * -sender {string} The userId of the person who sent the message
+     * -body {string} The body of the message the sender sent
+    */
+   process(conversationId, messages) {
+       messages.forEach(message => 
+        this.sentimentManger.process(this.language, message.body)
+        .then(result => {
+            this.addScore(conversationId, message.sender, result.score);
+        })
+        );
+    }
+    
+    /**
+     * Logs relevant sentiment stats regarding overall satisfaction 
+     * calculated 
+     */
+    logSentimentStats() {
+        console.log("--------Average Sentiment Per Conversation--------");
+        this.conversationScores.forEach((value, key) => {
+            const conversationScore = this.conversationScores.get(key);
+            const conversationVote = this.getSentimentVote(conversationScore.averageScore);
+            console.log(`Conversation [${key}] has an average ${conversationVote} sentiment of ${conversationScore.averageScore.toFixed()}`);
+            
+            conversationScore.userScores.forEach((value, key) => {
+                console.log(
+                    "\t----",
+                    this.getUserSentimentStat(conversationScore.userScores, key)
+                )
+            });
+        });
+        console.log();
 
+        console.log("--------Average Sentiment Per User--------");
+        this.userScores.forEach((value, key) => 
+            console.log(this.getUserSentimentStat(this.userScores, key))
+        );
+        console.log();
+
+        const averageSentiment = this.calcOverallAverageSentiment(this.sentimentScores);
+        console.log(`Average sentiment of dataset is ${this.getSentimentVote(averageSentiment)} with a score of ${averageSentiment.toFixed()}`);
+    }
+
+    // --------SENTIMENT STAT TRACKING HELPERS--------
     /**
      * Adds a sentiment score to the stat tracker by adding it to its
      * associated conversationScores and userScores.
@@ -107,27 +161,6 @@ class SentimentStatisticTracker {
         this.addUserScore(conversationScore.userScores, userId, newScore);
     }
 
-    /**
-     * Processes an array of messages for a conversation and tracks 
-     * the overall sentiment from the conversation and participants in 
-     * the conversation
-     * @param {string} conversationId The id of the conversation, 
-     * specifying the participants of the conversation as well as the 
-     * identifier for the conversation in this.conversationScores
-     * @param {Object[]} messages An array of message objects for the
-     * sentimentManager and SentimentStatTracker to process. Each
-     * member consists of
-     * -sender {string} The userId of the person who sent the message
-     * -body {string} The body of the message the sender sent
-     */
-    process(conversationId, messages) {
-        messages.forEach(message => 
-            this.sentimentManger.process(this.language, message.body)
-                .then(result => {
-                    this.addScore(conversationId, message.sender, result.score);
-                })
-        );
-    }
 
     /**
      * Calculates the running average of a set of user scores using the 
@@ -153,23 +186,6 @@ class SentimentStatisticTracker {
     }
 
     /**
-     * @returns {BigNumber} The average sentiment of sentimentScores
-     */
-    calcOverallAverageSentiment() {
-        return this.calcAverageSentiment(this.sentimentScores);
-    }
-
-    /**
-     * @param {Map} userScores A map of userScores to check from
-     * @param {string} userId The key value to check in userScores
-     * @returns {BigNumber} The average sentiment score for the userId in 
-     * userScores
-     */
-    getAverageSentimentForUser(userScores, userId) {
-        return userScores.get(userId).averageScore;
-    }
-
-    /**
      * Given an array of sentiment scores, returns that array's average
      * @param {BigNumber[]} sentiments An array of sentiment scores
      * @returns {BigNumber} The average sentiment of the sentiments
@@ -192,6 +208,24 @@ class SentimentStatisticTracker {
     }
 
     /**
+     * @returns {BigNumber} The average sentiment of sentimentScores
+     */
+    calcOverallAverageSentiment() {
+        return this.calcAverageSentiment(this.sentimentScores);
+    }
+
+    // --------SENTIMENT STAT LOGGING HELPERS--------
+    /**
+     * @param {Map} userScores A map of userScores to check from
+     * @param {string} userId The key value to check in userScores
+     * @returns {BigNumber} The average sentiment score for the userId in 
+     * userScores
+     */
+    getAverageSentimentForUser(userScores, userId) {
+        return userScores.get(userId).averageScore;
+    }
+
+    /**
      * Returns the vote of a sentiment score, whether or not a computed
      * sentiment was neutral, positive, or negative, based on senticon scoring,
      * the lexicon scoring system used by the SentimentManager to compute
@@ -206,36 +240,6 @@ class SentimentStatisticTracker {
     }
 
     /**
-     * Logs relevant sentiment stats regarding overall satisfaction 
-     * calculated 
-     */
-    logSentimentStats() {
-        console.log("--------Average Sentiment Per Conversation--------");
-        this.conversationScores.forEach((value, key) => {
-            const conversationScore = this.conversationScores.get(key);
-            const conversationVote = this.getSentimentVote(conversationScore.averageScore);
-            console.log(`Conversation [${key}] has an average ${conversationVote} sentiment of ${conversationScore.averageScore.toFixed()}`);
-            
-            conversationScore.userScores.forEach((value, key) => {
-                console.log(
-                    "\t----",
-                    this.getUserSentimentStat(conversationScore.userScores, key)
-                )
-            });
-        });
-        console.log();
-
-        console.log("--------Average Sentiment Per User--------");
-        this.userScores.forEach((value, key) => 
-            console.log(this.getUserSentimentStat(this.userScores, key))
-        );
-        console.log();
-
-        const averageSentiment = this.calcOverallAverageSentiment(this.sentimentScores);
-        console.log(`Average sentiment of dataset is ${this.getSentimentVote(averageSentiment)} with a score of ${averageSentiment.toFixed()}`);
-    }
-
-    /**
      * @param {Map} userScores A map of userScores to check from
      * @param {string} userId The key value to check in userScores
      * @returns {string} A string to log, outputting the average sentiment
@@ -246,7 +250,7 @@ class SentimentStatisticTracker {
         const userSentiment = this.getAverageSentimentForUser(userScores, userId);
         const userVote = this.getSentimentVote(userSentiment);
 
-        return `User of id [${userId}] has a ${userVote} of ${userSentiment.toFixed()}`;
+        return `User of id [${userId}] has a ${userVote} sentiment of ${userSentiment.toFixed()}`;
     }
 }
 
